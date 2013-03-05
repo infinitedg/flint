4b33ee5729d7855ed28f55e89a9902ad1c0a4bcc
Brent Anderson <me@brentjanderson.com>
2013-03-04 22:10:56 -0700

    Working on view composition hierarchy so that we can use routes and embed cards within stations within the entire rendered framework.

--- client/js/templates/main.js
+++ client/js/templates/main.js
@@ -1,7 +1,7 @@
 (function () {
 	'use strict';
 	
-    Template.coreController.events({
+    Template.stationPicker.events({
 		'click button.simulator': function(event) {
 			App.beep();
 			Session.set('selectedSimulator', this);
@@ -9,7 +9,6 @@
 		'click button.station': function(event) {
 			Session.set('selectedStation', this);
 			Cookie.set('station', this._id);
-			Meteor.Router.to('/');
 			window.location.reload();
 		},
     });
@@ -34,6 +33,29 @@
 	
 	Template.stationPicker.selectedSimulator = function() {
 		return Session.get('selectedSimulator');
+	}
+	
+	Template.stationCore.simulator = function() {
+		var station = Stations.findOne({_id: Cookie.get('station')});
+		if (station) {
+			var simulator = Simulators.findOne({_id: station.simulator});
+			return simulator;
+		} else {
+			return {};
+		}
+	}
+	
+	Template.stationCore.station = function() {
+		return Stations.findOne({_id: Cookie.get('station')});
+	}
+	
+	Template.cardList.cards = function() {
+		var station = Stations.findOne({_id: Cookie.get('station')});
+		if (station) {
+			return station.cards;
+		} else {
+			return [];
+		}
 	}
 	
 }());
