c46bc0d31a2a3043964108ef5d1d666c3e1cf480
Brent Anderson <me@brentjanderson.com>
2013-03-05 19:24:50 -0700

    Forgot to run JSHint. Replaced spaces with tabs.

--- server/server.js
+++ server/server.js
@@ -7,66 +7,66 @@ Meteor.startup(function () {
 		});
 		
 		Simulators.insert({
-		    "name": "USS Phoenix",
-		    "_id": "dX2EuPNoSA2tmBBbH"
+			"name": "USS Phoenix",
+			"_id": "dX2EuPNoSA2tmBBbH"
 		});
 		
 		Simulators.insert({
-		    "_id": "8ofeYktqCx3SEGgpx",
-		    "name": "USS Voyager",
-		    "power": 120
+			"_id": "8ofeYktqCx3SEGgpx",
+			"name": "USS Voyager",
+			"power": 120
 		});
 		
 		Stations.insert({
-		    "_id": "N9h4coBbhLTHHEYud",
-		    "cards": [
-		        {
-		            "name": "Login",
-		            "cardId": "login"
-		        },
-		        {
-		            "name": "Power Distribution",
-		            "cardId": "power"
-		        },
-		        {
-		            "cardId": "alertCondition",
-		            "name": "Alert Condition"
-		        }
-		    ],
-		    "name": "Operations",
-		    "simulator": "8ofeYktqCx3SEGgpx"
+			"_id": "N9h4coBbhLTHHEYud",
+			"cards": [
+				{
+					"name": "Login",
+					"cardId": "login"
+				},
+				{
+					"name": "Power Distribution",
+					"cardId": "power"
+				},
+				{
+					"cardId": "alertCondition",
+					"name": "Alert Condition"
+				}
+			],
+			"name": "Operations",
+			"simulator": "8ofeYktqCx3SEGgpx"
 		});
 		
 		Systems.insert({
-		    "name": "Short Range Communications",
-		    "minPower": 5,
-		    "maxPower": 20,
-		    "power": 7,
-		    "_id": "nprnQfbti8XBbQ42W"
+			"name": "Short Range Communications",
+			"minPower": 5,
+			"maxPower": 20,
+			"power": 7,
+			"_id": "nprnQfbti8XBbQ42W"
 		});
 		
 		Systems.insert({
-		    "name": "Torpedo Launchers",
-		    "minPower": 7,
-		    "maxPower": 30,
-		    "power": 10,
-		    "_id": "8DnWtrhdxC55PcY9a"
+			"name": "Torpedo Launchers",
+			"minPower": 7,
+			"maxPower": 30,
+			"power": 10,
+			"_id": "8DnWtrhdxC55PcY9a"
 		});
 		
 		Systems.insert({
-		    "name": "Phasers",
-		    "minPower": 7,
-		    "maxPower": 30,
-		    "power": 10,
-		    "_id": "erisBfgr5uC568EZj"
+			"name": "Phasers",
+			"minPower": 7,
+			"maxPower": 30,
+			"power": 10,
+			"_id": "erisBfgr5uC568EZj"
 		});
 		
 		Systems.insert({
-		    "name": "Warp Core",
-		    "minPower": 5,
-		    "maxPower": 20,
-		    "power": 7,
-		    "_id": "bPob55YS8CPcnSr6w"
+			"name": "Warp Core",
+			"minPower": 5,
+			"maxPower": 20,
+			"power": 7,
+			"_id": "bPob55YS8CPcnSr6w"
 		});
 	}
 });
