392dd09f4370bf53de48e2f0a0548192d601a406
Brent Anderson <me@brentjanderson.com>
2013-03-18 23:37:43 -0600

    Improve manual sensor animation loop.
    Removed global scope of certain variables (was used for testing). Also added object removal when it passes beyond the edge of the sensors grid. Minor improvements to interface, sample icon.

--- cards/sensorGrid/client/js/card.js
+++ cards/sensorGrid/client/js/card.js
@@ -3,17 +3,16 @@
   
   var k;
   var observeLoop;
-  var sensorContactAnimationLoop; // @TODO move back to function scope
+  var sensorContactAnimationLoop; 
   
-  window.sensorContacts = {}; // @TODO move back to function scope
+  var sensorContacts = {};
   
   Template.card_sensorGrid.sensorInfo = function() {
     return Flint.getSimulator().sensorText;
   };
   
-  Template.card_sensorGrid.rendered = function() {
+  Template.card_sensorGrid.created = function() {
     Meteor.defer(function(){
-      
       k = {
         width: 600,
         height: 600,
@@ -26,8 +25,8 @@
         y: k.height / 2
       };
       k.radius = (k.width / 2 < k.height / 2) ? k.width / 2 - k.strokeWidth : k.height / 2 - k.strokeWidth;
-      
-      window.stage = new Kinetic.Stage({ // @TODO move back to function scope
+    
+      var stage = new Kinetic.Stage({
         container: k.container,
         width: k.width,
         height: k.height
@@ -41,7 +40,7 @@
         stroke: k.color,
         strokeWidth: k.strokeWidth
       });
-    
+  
       var middleCircle = new Kinetic.Circle({
         x: stage.getWidth() / 2,
         y: stage.getHeight() / 2,
@@ -49,7 +48,7 @@
         stroke: k.color,
         strokeWidth: k.strokeWidth
       });
-    
+  
       var innerCircle = new Kinetic.Circle({
         x: stage.getWidth() / 2,
         y: stage.getHeight() / 2,
@@ -57,7 +56,7 @@
         stroke: k.color,
         strokeWidth: k.strokeWidth
       });
-    
+  
       // Draw lines
       var linepoints = function(angle, inverted) {
         // angle in degrees converted to radians
@@ -68,7 +67,7 @@
         var y1 = k.center.y + k.radius * Math.sin(angle);
         var x2 = k.center.x + k.radius * Math.cos(angle);
         var y2 = k.center.y - k.radius * Math.sin(angle);
-      
+    
         // inverted flips the line across the horizontal axis
         if (inverted) {
           var iy1 = y2;
@@ -78,7 +77,7 @@
         }
         return [[x1, y1], [x2, y2]];
       };
-   
+ 
       /// lineHigh a line through the origin terminating on the circle at 75 degrees
       var linePrototype = {
         stroke: k.color,
@@ -86,91 +85,98 @@
         lineCap: 'round',
         lineJoin: 'round'
       };
-      
-      var lineHigh = new Kinetic.Line(_.extend(linePrototype, {points: linepoints(75)}));
     
+      var lineHigh = new Kinetic.Line(_.extend(linePrototype, {points: linepoints(75)}));
+  
       /// lineMid a line through the origin terminating on the circle at 45 degrees
       var lineMid = new Kinetic.Line(_.extend(linePrototype, {points: linepoints(45)}));
-    
+  
       /// lineLow a line through the origin terminating on the circle at 15 degrees
       var lineLow = new Kinetic.Line(_.extend(linePrototype, {points: linepoints(15)}));
-    
+  
       /// lineHighInv a line through the origin terminating on the circle at 75 degrees
       var lineHighInv = new Kinetic.Line(_.extend(linePrototype, {points: linepoints(75, true)}));
-    
+  
       /// lineMidInv a line through the origin terminating on the circle at 45 degrees
       var lineMidInv = new Kinetic.Line(_.extend(linePrototype, {points: linepoints(45, true)}));
 
       /// lineLowInv a line through the origin terminating on the circle at 15 degrees
       var lineLowInv = new Kinetic.Line(_.extend(linePrototype, {points: linepoints(15, true)}));
-    
+  
       // Create backdrop for sensors grid
       var backdrop = new Kinetic.Layer();
-    
+  
       // add the shape to the layer
       backdrop.add(outerCircle);
       backdrop.add(middleCircle);
       backdrop.add(innerCircle);
-    
+  
       backdrop.add(lineHigh);
       backdrop.add(lineMid);
       backdrop.add(lineLow);
-    
+  
       backdrop.add(lineHighInv);
       backdrop.add(lineMidInv);
       backdrop.add(lineLowInv);
     
+      Flint.Log.verbose('Base grid drawn', 'Sensors');
+  
       // Setup sensor contacts
       var contactsLayer = new Kinetic.Layer();
       var addContact = function(contact) {
         if (sensorContacts[contact._id] === undefined) {
-          console.log('added',contact);
-          var circle = new Kinetic.Circle({
-            x: contact.x * k.width,
-            y: contact.y * k.height,
-            radius: 20,
-            fill: 'red',
-            stroke: 'black',
-            strokeWidth: 4
-          });
-      
-          contactsLayer.add(circle);
-          contact._sprite = circle;
+          Flint.Log.verbose('Added contact ' + contact._id, 'Sensors');
+        
+          var imageObj = new Image();
+          imageObj.onload = function() {
+            var image = new Kinetic.Image({
+              x: contact.x * k.width,
+              y: contact.y * k.height,
+              image: imageObj,
+              width: 32,
+              height: 32
+            });
+    
+            contactsLayer.add(image);
+            sensorContacts[contact._id]._sprite = image;
+          };
         
           sensorContacts[contact._id] = contact;
         
-          stage.draw();
+          imageObj.src = '/cards/sensorGrid/sprites/planet.big';
         }
       };
-      
+    
       // Add currently available contacts to the simulation
       SensorContacts.find().forEach(addContact);
-      
+    
       // Observe changes to collection
       if (observeLoop === undefined) {
         observeLoop = SensorContacts.find().observe({
           added: addContact,
           changed: function(contact, oldContact) {
-            console.log("changed", contact);
+            Flint.Log.verbose('Changed contact ' + contact._id, 'Sensors');
             var sprite = sensorContacts[contact._id]._sprite;
             contact._sprite = sprite;
             sensorContacts[contact._id] = contact;
           },
           removed: function(contact) {
-            console.log("removed", contact);
+            Flint.Log.verbose('Removed contact ' + contact._id, 'Sensors');
             sensorContacts[contact._id]._sprite.remove();
             delete sensorContacts[contact._id];
             stage.draw();
           }
         });
       }
-      
-      // 
-      var flipper = 0;
-      window.sensorContactAnimationLoop = new Kinetic.Animation(function(frame) {
+    
+      sensorContactAnimationLoop = new Kinetic.Animation(function(frame) {
         var dt = frame.timeDiff / 1000; // Change in time between this frame and the prior frame
-        
+      
         for (var i in sensorContacts) {
+          if (sensorContacts[i]._sprite === undefined) { // Ignore contacts with undefined sprites - they may be loading
+            continue;
+          }
+        
           var contact = sensorContacts[i];
           var sprite = contact._sprite;
           var v = contact.velocity || 5; // Defaults to 1 pixels/second (as a fraction of a grid)
@@ -178,24 +184,24 @@
           var y0 = sprite.getY();
           var x  = contact.x * k.width;
           var y  = contact.y * k.height;
-          
+        
           if (x0 === x && y0 === y) {
             sensorContacts[i].isMoving = false;
             continue;
           }
-          
+        
           // First, if the velocity is zero then move the contact there immediately
           sensorContacts[i].isMoving = true;
           if (v === 0) {
             sensorContacts[i]._sprite.setPosition(x, y);
             continue;
           }
-          
+        
           // 1. Calculate the distance in x, y, and direct (h)
           var dx = Math.abs(x - x0); // Distance between current sprite location and intended location on the x-axis
           var dy = Math.abs(y - y0); // Distance between current sprite location and intended location on the y-axis
           var h  = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2)); // Distance between current sprite location and intended location (the hypotenuse)
-          
+        
           // 2. Calculate angle theta
           var th  = Math.acos(dx / h);
 
@@ -205,13 +211,13 @@
           } else {
             x1 = x0 - v * dt * Math.cos(th);
           }
-          
+        
           if (y0 - y < 0) {
             y1 = y0 + v * dt * Math.sin(th);
           } else {
             y1 = y0 - v * dt * Math.sin(th);
           }
-          
+        
           // If the difference between the new point and the target point is within our threshhold, then set it to the new location
           var threshhold = 0.2;
           if (Math.abs(x1-x) < threshhold) {
@@ -220,26 +226,26 @@
           if (Math.abs(y1-y) < threshhold) {
             y1 = y;
           }
-          
+        
           // 3. Set the location of this sprite to the new location
           sensorContacts[i]._sprite.setPosition(x1, y1);
-        }
-        if (window.sensorContactAnimationLoop !== undefined && flipper !== 0) {
-          // window.sensorContactAnimationLoop.stop();
-          flipper = 0;
-        } else {
-          flipper = 1;
-        }
         
+          // If the sprite is out of bounds, then remove it
+          if (Math.sqrt(Math.pow(x1 - k.center.x, 2) + Math.pow(y1 - k.center.y, 2)) > k.radius - k.strokeWidth) {
+            SensorContacts.remove({_id: i});
+          }
+        }
       }, contactsLayer);
-      
+    
       // add the layer to the stage
       stage.add(backdrop);
       stage.add(contactsLayer);
-      
+    
+      Flint.Log.verbose('Layers attached to stage', 'Sensors');
+    
       // Start our animations
-      window.sensorContactAnimationLoop.start();
-      
+      sensorContactAnimationLoop.start();
+      Flint.Log.verbose('Animation loop started', 'Sensors');
     });
   };
 }());
