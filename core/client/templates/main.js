77587a49298676cd2f97639f4de974b70e013ea8
Brent Anderson <me@brentjanderson.com>
2013-03-20 17:54:06 -0600

    Add offline notification. Closes #9.
    Meteor.status() information added to the bottom of the screen when the server is offline or unavailable. Includes a retry counter indicating how many attempts have been made, how many seconds until the next update, and a button to attempt to reconnect immediately.
    
    Future improvements may include a live counter actually counting down to when the reconnect will happen.

--- core/client/templates/main.js
+++ core/client/templates/main.js
@@ -54,6 +54,34 @@
     return Session.get('_programming');
   };
   
+  Template.programming_tools.offline = function() {
+    var r = Meteor.status();
+    return !r.connected;
+  };
+  
+  Template.programming_tools.offlineMessage = function() {
+    var r = Meteor.status();
+    return r.reason;
+  };
+  
+  Template.programming_tools.offlineStatus = function() {
+    var r = Meteor.status();
+    return r.status;
+  };
+  
+  Template.programming_tools.offlineCounting = function() {
+    var r = Meteor.status();
+    var s = 'Retried ' + r.retryCount + ' times - next attempt in ' + Math.round((r.retryTime - (new Date()).getTime()) / 1000) + ' seconds';
+    return s;
+  };
+  
+  Template.programming_tools.events = {
+    'click a': function(e, t) {
+      Meteor.reconnect();
+      e.preventDefault();
+    }
+  };
+  
   Template.themePicker.theme = function() {
     var station = Stations.findOne({_id: Session.get('station')});
     if (station) {
