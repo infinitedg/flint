1b2fd3914e24b7917c27dfd72e2f425d6d99ee97
Brent Anderson <me@brentjanderson.com>
2013-06-25 22:26:53 -0600

    Improvement to voice management - don't load speech configuration (very heavy on memory) until we actually try to say something.

--- core/voice/voice.js
+++ core/voice/voice.js
@@ -11,6 +11,13 @@ Flint = this.Flint || {};
     // Function to read text aloud
     // This will break someday when google shuts this down. If we can find a better component for this that would be ideal.
     say: function(txt) {
+      if (meSpeak.isConfigLoaded()) {
+        // Trigger only when we actually use it
+        meSpeak.loadConfig(voicePrefix + "mespeak_config.json");
+        Flint.loadVoice('en-us');
+        Flint.Log.verbose('MeSpeak loaded','Speech');
+      }
+      
       if (spokenPhrases[txt] === undefined || new Date().getTime() - spokenPhrases[txt] >= 1000) {
         Flint.Log.verbose('Said "'+txt+'"', 'Speech');
         meSpeak.speak(txt);
@@ -22,10 +29,4 @@ Flint = this.Flint || {};
     }
   });
   
-  // Trigger when everything is loaded
-  Meteor.startup(function(){
-    meSpeak.loadConfig(voicePrefix + "mespeak_config.json");
-    Flint.loadVoice('en-us');
-    Flint.Log.verbose('MeSpeak loaded','Speech');
-  });
 }());
