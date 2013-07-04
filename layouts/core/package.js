77bbb343824a4eb29914aed9eded1390ccf5e6cb
Brent Anderson <me@brentjanderson.com>
2013-07-03 19:17:17 -0600

    Removed dependency on jquery-masonry from project.

--- layouts/core/package.js
+++ layouts/core/package.js
@@ -3,7 +3,7 @@ Package.describe({
 });
 
 Package.on_use(function(api) { 
-  api.use(['templating', 'flint-core', 'flint-ui', 'log', 'jquery-masonry']);
+  api.use(['templating', 'flint-core', 'flint-ui', 'log']);
   
   api.add_files(['template.html', 'core.js'], 'client');
 });
