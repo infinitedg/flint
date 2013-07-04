d1aa596a865a5c8477df758d7e1abe1325e8d4e6
Brent Anderson <me@brentjanderson.com>
2013-07-04 13:41:30 -0600

    Basic implementation of YUIDoc. Refs #15.

---
+++ docs_core_functionality.js
@@ -0,0 +1,54 @@
+// A file purely for declaring descriptions for classes, modules, and other globally avilable tags in YUIDoc
+
+/**
+Core functionality provided to the Flint platform
+@module Core Functionality
+*/
+
+/**
+ * Static class with centralized convenience functions for Flint
+ * @class Flint
+ * @static
+*/
+
+/**
+* Provides various utility functions
+* @class Utils
+* @static
+*/
+
+/**
+List of collections available via `Flint.collection(__collectionName__)`. Not really a class, just a convenient way to render our documentation.
+@class Flint.collections
+*/
+
+/**
+Handlebars helper functions. Not really a class, just a convenient way to render our documentation.
+@class Handlebars.helpers
+*/
+
+/**
+* Client methods available via `Meteor.call`. Not really a class, just a convenient way to render our documentation.
+* @class Meteor.call
+*/
+
+/**
+Documentation for `Meteor.startup` functions to track various behaviors when running the simulator. Not really a class, just a convenient way to render our documentation.
+@class Meteor.startup
+*/
+
+
+/**
+Filters for [Meteor.Router](atmosphere.meteor.com/package/router). Not really a class, just a convenient way to render our documentation.
+@class Router.Filters
+*/
+
+/**
+Core routes for [`Meteor.Router`](https://atmosphere.meteor.com/package/router). Not really a class, just a convenient way to render our documentation.
+@class Router.Routes
+*/
+
+/**
+ Core Flint subscriptions for [`Meteor.subscribe`](http://docs.meteor.com/#meteor_subscribe). Not really a class, just a convenient way to render our documentation.
+ @class Subscriptions
+*/
---
+++ docs_templates.js
@@ -0,0 +1,10 @@
+/**
+All Templates used by the Flint platform
+@module Templates
+*/
+
+/**
+Cards employed by Flint
+@module Templates
+@submodule Cards
+*/
