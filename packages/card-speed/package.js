df6d0fd80a4e570e7afc3a2119c2e1befacea962
Brent Anderson <me@brentjanderson.com>
2014-01-11 17:33:39 +0000

    Created basic card-speed package, cleaned up old card-simSpeed package.

--- packages/card-speed/package.js
+++ packages/card-speed/package.js
@@ -1,8 +1,9 @@
 Package.describe({
+  summary: "Controls the simulator's speed"
 });
 
 Package.on_use(function(api) {  
-  api.use(['templating', 'flint-core', 'flint-ui']);
+  api.use(['templating', 'flint']);
   
   api.add_files(['core.html', 'core.js'], 'client');
   api.add_files(['card.html', 'card.js'], 'client');
