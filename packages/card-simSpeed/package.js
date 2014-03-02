5fd6ea9c1041456a9f4aa0757df1d2de92da5771
Brent Anderson <me@brentjanderson.com>
2014-03-01 21:50:32 -0700

    Corrections to errors in the simspeed package

--- packages/card-simSpeed/package.js
+++ packages/card-simSpeed/package.js
@@ -2,7 +2,7 @@ Package.describe({
 });
 
 Package.on_use(function(api) {  
-  api.use(['templating', 'flint-core', 'flint-ui']);
+  api.use(['templating', 'flint']);
   
   api.add_files(['core.html', 'core.js'], 'client');
   api.add_files(['card.html', 'card.js'], 'client');
