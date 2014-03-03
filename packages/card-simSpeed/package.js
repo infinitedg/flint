61580e00cacd9a540625b42a4884ef3e5da5c1fe
Alex <alexanderson1993@gmail.com>
2014-03-03 14:46:43 -0700

    Got rid of this card, coz it breaks stuff

--- packages/card-simSpeed/package.js
+++
@@ -1,9 +0,0 @@
-Package.describe({
-});
-
-Package.on_use(function(api) {  
-  api.use(['templating', 'flint-core', 'flint-ui']);
-  
-  api.add_files(['core.html', 'core.js'], 'client');
-  api.add_files(['card.html', 'card.js'], 'client');
-});
