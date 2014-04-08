37687f4061abd998e9fbb38c11bed235cb164140
Alex <alexanderson1993@gmail.com>
2014-04-08 00:03:52 -0600

    Added Three.js. Included a couple of packages for content right off the bat.
    To make it work, I did some changes to the three.js file in the three.js package, which is downloaded by Meteorite. 1st updated the file from threejs.com. 2nd added 'window.THREE = THREE' near the top of the file.
    
    Added the model into the thruster screen and made it move when the thrusters are fired.

--- packages/card-thrusters/card.js
+++ packages/card-thrusters/card.js
@@ -2,12 +2,83 @@
 @module Templates
 @submodule Cards
 */
- 
+
+var convertToRadian = function(degrees){
+    return degrees * (Math.PI/180);
+}
 /**
 Card for manipulating the thrusters. Also shows ship orientation (Yaw, pitch roll).
 @class card_thrusters
 */
-
+Template.card_thrusters.created = function() {
+    var renderer	= new THREE.WebGLRenderer();
+	renderer.setSize( 500, 375 );
+	document.getElementsByClassName('card-area').item().appendChild( renderer.domElement );
+    var scene	= new THREE.Scene();
+	var camera	= new THREE.PerspectiveCamera(45, 500 / 375, 0.01, 10000);
+    
+    var onRenderFcts= [];
+    
+    var ambientLight= new THREE.AmbientLight( 0x020202 );
+	scene.add( ambientLight);
+	var frontLight	= new THREE.DirectionalLight('white', 1);
+	frontLight.position.set(0.5, 0.5, 2);
+	scene.add( frontLight );
+	var backLight	= new THREE.DirectionalLight('white', 0.75);
+	backLight.position.set(-0.5, -0.5, -2);
+	scene.add( backLight );
+    
+    var geometry  = new THREE.SphereGeometry(900, 32, 32);
+    var url   = '/packages/card-thrusters/threeRequires/images/galaxy_starfield.png';
+    var material  = new THREE.MeshBasicMaterial({
+        map : THREE.ImageUtils.loadTexture(url),
+        side  : THREE.BackSide
+    });
+    var starSphere  = new THREE.Mesh(geometry, material);
+    scene.add(starSphere);
+    
+   var spaceship   = null;
+        THREEx.SpaceShips.loadSpaceFighter03(function(object3d){
+        spaceship   = object3d;
+        spaceship.rotateY(Math.PI/2);
+        scene.add(spaceship);
+    })
+    onRenderFcts.push(function(){
+       spaceship.rotation.y = -1 * convertToRadian(Flint.simulator('thrusterRotationYaw'));
+        spaceship.rotation.x = -1 * convertToRadian(Flint.simulator('thrusterRotationPitch'));
+        spaceship.rotation.z = -1 * convertToRadian(Flint.simulator('thrusterRotationRoll'));
+          
+    })
+        
+    var mouse	= {x : 0, y : 0}
+	document.addEventListener('mousemove', function(event){
+		mouse.x	= (event.clientX / window.innerWidth ) - 0.5;
+		mouse.y	= (event.clientY / window.innerHeight) - 0.5;
+	}, false)
+	onRenderFcts.push(function(delta, now){
+		camera.position.x = spaceship.position.x; // + ((mouse.x*10 - camera.position.x) * (delta*3))
+		camera.position.y = spaceship.position.y + 2;// + ((mouse.y*10 - camera.position.y) * (delta*3))
+        camera.position.z = spaceship.position.z + 3;
+		camera.lookAt( spaceship.position );
+	})
+    
+    onRenderFcts.push(function(){
+		renderer.render( scene, camera );		
+	})
+    var lastTimeMsec= null
+	requestAnimationFrame(function animate(nowMsec){
+		// keep looping
+		requestAnimationFrame( animate );
+		// measure time
+		lastTimeMsec	= lastTimeMsec || nowMsec-1000/60;
+		var deltaMsec	= Math.min(200, nowMsec - lastTimeMsec);
+		lastTimeMsec	= nowMsec;
+		// call each update function
+		onRenderFcts.forEach(function(onRenderFct){
+			onRenderFct(deltaMsec/1000, nowMsec/1000);
+		});
+	});
+}
 
 Template.card_thrusters.events = {
   /**
