0fcf4b2197aa97961717472719f37925430743c8
Alex <alexanderson1993@gmail.com>
2014-04-25 20:40:35 -0600

    A bit of work on the Message Composer. It's pretty much working right.

--- packages/card-messageComposer/card.js
+++ packages/card-messageComposer/card.js
@@ -4,8 +4,7 @@
 //To - the 'To' field
 //Body - the 'Body' field
 //Endcoded - if this key is set, the message appears as binary and cannot be edited
-//Status - if the message has been sent and is visible to the Flight Director, or if it has been queued.
-
+//Status - if the message has been sent and is visible to the Flight Director, or if it has been queued.  
 Template.card_messageComposer.events = {
     'click .encodeBtn' : function(e, context) {
             str = context.find('.body-text').value;
@@ -22,7 +21,7 @@ Template.card_messageComposer.events = {
                     if (j > i) {Meteor.clearInterval(encoder);}
                 },10)
             $(context.find('.body-text')).attr('disabled', 'disabled');
-            Flint.collection('lrmessages').upsert(Session.get('currentLRMessage'),{$set: {encoded: true}});
+            Flint.collection('lrmessages').update(Session.get('currentLRMessage'),{$set: {encoded: true}});
  
     },
     
@@ -35,15 +34,11 @@ Template.card_messageComposer.events = {
         Template.card_messageComposer.saveMessage(t);
         $(t.findAll('.message-list .textbox p')).removeClass('lineSelected');
         t.find('.to-input').value = this.to;
-        t.find('.body-text').value = this.body;
+        if (this.body != 'undefined'){
+        t.find('.body-text').value = this.body;}
         $(e.target).addClass('lineSelected');
-        if (this.encoded){
-            console.log('encoded!');
-            $(t.find('.body-text')).attr('disabled', 'disabled');
-        } else {
-            console.log('Not encoded!');
-            $(t.find('.body-text')).removeAttr('disabled');
-        }
+        $(t.find('.to-input')).removeAttr('disabled');
+        $(t.find('.body-text')).removeAttr('disabled');
         Session.set('currentLRMessage',this._id);
     },
     'click .newBtn' : function(e, t) {
@@ -54,43 +49,69 @@ Template.card_messageComposer.events = {
             to: '<<NEW MESSAGE>>',
             body: ''.value,
             encoded: false,
-            status: '',
+            status: 'draft',
             simulatorId: Flint.simulatorId()
           });
         t.find('.to-input').value = '<<NEW MESSAGE>>';
         t.find('.body-text').value = '';
+        $(t.find('.body-text')).removeAttr('disabled');
+        $(t.find('.to-input')).removeAttr('disabled');
         Session.set('currentLRMessage',messageID);
    },
+    'click .queueBtn' : function(e, t) {
+        Flint.collection('lrmessages').update(Session.get('currentLRMessage'),{$set: {status: 'queued'}});
+        t.find('.to-input').value = '';
+        t.find('.body-text').value = '';
+        $(t.find('.body-text')).removeAttr('disabled');
+        $(t.find('.body-text')).attr('disabled', 'disabled');
+        $(t.find('.to-input')).attr('disabled', 'disabled');
+        Session.set('currentLRMessage','');
+        
+    },
     'click .clearBtn' : function(e, t) {
         var messageID = Session.get('currentLRMessage');
         Flint.collection('lrmessages').remove(messageID);
         t.find('.to-input').value = '';
         t.find('.body-text').value = '';
-        $(t.find('.body-text')).removeAttr('disabled');
+        $(t.find('.body-text')).attr('disabled', 'disabled');
+        $(t.find('.to-input')).attr('disabled', 'disabled');
         Session.set('currentLRMessage','');
-   }
+   },
+    'blur .body-text, blur .to-input' : function(e, t) {
+        Template.card_messageComposer.saveMessage(t);
+    }
 };
 
 Template.card_messageComposer.saveMessage = function(context){
+    if (Session.get('currentLRMessage') == '') {return null;}
     var message = Flint.collection('lrmessages').find({_id: Session.get('currentLRMessage')}).fetch();
     var senderName = Flint.station().name;
+    console.log(message);
+    if (message.encoded == true) { var bodyText = message.body;}
+    else {var bodyText = context.find('.body-text').value;}
+    var toText = context.find('.to-input').value;
+    if (bodyText == null) {bodyText = '';}
+    if (toText == null || toText == '') {toText == '<<NEW MESSAGE>>';}
     Flint.collection('lrmessages').update(
         Session.get('currentLRMessage'),
         {
             sender: senderName,
-            to: context.find('.to-input').value,
-            body: context.find('.body-text').value,
+            to: toText,
+            body: bodyText,
             encoded: message.encoded,
-            status: message.status,
+            status: 'draft',
             simulatorId: Flint.simulatorId()
         });
 };
+
+
 /**
 Create a subscripton to cards.messageComposer.lrmessages and save for later teardown
 @method created
 */
 Template.card_messageComposer.created = function() {
-  this.subComputation = Deps.autorun(function() {
+    Session.set('currentLRMessage','');
+    this.subComputation = Deps.autorun(function() {
     Meteor.subscribe("cards.messageComposer.lrmessages", Flint.simulatorId());
   });
 };
@@ -101,12 +122,16 @@ Teardown subscription to cards.messageComposer.lrmessages
 */
 Template.card_messageComposer.destroyed = function() {
   this.subComputation.stop();
+    //Session.set('currentLRMessage','');
 };
 
 Template.card_messageComposer.messageList = function(){
-    return Flint.collection('lrmessages').find();
+    return Flint.collection('lrmessages').find({status: 'draft', sender: Flint.station().name, simulatorId: Flint.simulatorId()});
 };
 function setCharAt(str,index,chr) {
     if(index > str.length-1) return str;
     return str.substr(0,index) + chr + str.substr(index+1);
 };
+Template.card_login.alertLevel = function() {
+ return Template.layout_default.alertLevel();   
+}
