bbbfd25b0c5803afdc51e055b5e99ed1b0cf21ff
Alex <alexanderson1993@gmail.com>
2014-04-25 20:43:33 -0600

    Added some card.

---
+++ packages/card-shortRangeComm/package.js
@@ -0,0 +1,9 @@
+Package.describe({
+});
+
+Package.on_use(function(api) {  
+  api.use(['templating', 'flint']);
+  
+  api.add_files(['core.html', 'core.js'], 'client');
+  api.add_files(['card.html', 'card.js', 'card.css'], 'client');
+});
