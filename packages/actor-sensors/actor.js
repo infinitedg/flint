9191907217f623a71db59f7f4ebd53f76db30a79
Brent Anderson <me@brentjanderson.com>
2014-05-07 22:45:20 -0600

    Rework of Actor API to handle multiple contacts better, remove contacts when at 120% of radius, and to otherwise perform well.
    This actor will be ported to GSAP and we will discontinue Tween.js for Flint. GSAP supports a number of advanced tweening features that are necessary for sensors to have all the functionality we want/need, including in-flight changes to destination Tweens. We may still end up writing our own physics engine, but hopefully GSAP can handle what we need from it in the meantime.

--- packages/actor-sensors/actor.js
+++ packages/actor-sensors/actor.js
@@ -1,5 +1,4 @@
-var contactTweens = {},
-defaultVelocity = 0.01;
+var defaultVelocity = 0.01;
 
 var a = Flint.actor({
 	_id:"actor-sensors",
@@ -15,46 +14,53 @@ var a = Flint.actor({
 		// Flint.Log.info("actor-sensors killed", "actor-sensors");
 	}, onError: function(exc){
 		// Flint.Log.error("actor-sensors error", "actor-sensors");
-		// Flint.Log.data(exc, "actor-sensors");
-		Flint.Log.verbose(exc);
+		Flint.Log.data(exc, "actor-sensors");
 	}
 });
 
+function findTweenById(id) {
+	return _.where(TWEEN.getAll(), {contactID: id})[0];
+}
+
 var updateTweener = function(doc) {
-	if (contactTweens[doc._id]) {
-		contactTweens[doc._id].stop();
-		delete contactTweens[doc._id];
-	}
+	TWEEN.remove(findTweenById(doc._id));
 	var dist = Math.sqrt(Math.pow(doc.x - doc.dstX, 2) + Math.pow(doc.y - doc.dstY, 2) + Math.pow(doc.z - doc.dstZ, 2)),
 	velocity = doc.velocity || defaultVelocity;
-	contactTweens[doc._id] = new TWEEN.Tween({x: doc.x, y: doc.y, z: doc.z})
+	var t = new TWEEN.Tween({x: doc.x, y: doc.y, z: doc.z})
 		.to({x: doc.dstX, y: doc.dstY, z: doc.dstZ }, Math.round(1000 * dist / velocity))
 		.easing(TWEEN.Easing.Linear.None)
 		.onUpdate( function() {
-			Flint.collection('sensorContacts').update(doc._id, { $set: {x: this.x, y: this.y, z: this.z}});
+			var d = Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2) + Math.pow(this.z, 2));
+			if (d > 1.2) {
+				Flint.collection('sensorContacts').remove(doc._id);
+			} else {
+				Flint.collection('sensorContacts').update(doc._id, { $set: {x: this.x, y: this.y, z: this.z}});
+			}
+		})
+		.onStart(function() {
+			this.contactID = doc._id;
 		})
 		.onComplete(function() {
 			Flint.collection('sensorContacts').update(doc._id, {$set: {isMoving: false}});
-			delete contactTweens[doc._id];
 		})
 		.start();
 }
 
 var observer = Flint.collection('sensorContacts').find({isMoving: true}).observe({
 	added: function(doc) {
+		// console.log("Added",doc._id);
 		if (doc.x !== doc.dstX || doc.y !== doc.dstY || doc.z !== doc.dstZ) {
 			updateTweener(doc);
 		}
 	},
 	changed: function(newDoc, oldDoc) {
 		if (newDoc.dstX !== oldDoc.dstX || newDoc.dstY !== oldDoc.dstY || newDoc.dstZ !== oldDoc.dstZ || newDoc.velocity !== oldDoc.velocity) {
+			// console.log("Changed",newDoc._id, newDoc);
 			updateTweener(newDoc);
 		}
 	},
 	removed: function(doc) {
-		if (contactTweens[doc._id]) {
-			contactTweens[doc._id].stop();
-			delete contactTweens[doc._id];
-		}
+		// console.log("Removed",doc._id);
+		TWEEN.remove(findTweenById(doc._id));
 	}
 });
--- packages/actor-sensors/package.js
+++ packages/actor-sensors/package.js
@@ -3,7 +3,7 @@ Package.describe({
 });
 
 Package.on_use(function(api) { 
-  api.use(['flint', 'flint-drama', 'tween']);
+  api.use(['flint', 'flint-drama', 'tween', 'underscore']);
    
   api.add_files(['actor.js'], ['server']);
 });
