e7192de6b0d679ab3c296ea9784c1aecef7d9d61
Brent Anderson <me@brentjanderson.com>
2014-05-07 22:41:07 -0600

    Fix bug in actor API so that the API can support multiple actors.

--- packages/flint-drama/drama.js
+++ packages/flint-drama/drama.js
@@ -1,4 +1,4 @@
 var dramaInterval = Meteor.setInterval(function() {
-	Flint.collection('FlintActors').update({}, {$inc: {counter: 1}});
+	Flint.collection('FlintActors').update({}, {$inc: {counter: 1}}, {multi: true});
 	Flint.collection('FlintActors').update({counter: {$gte: 5}}, {$set: {claimedBy: DramaInstanceID}});
 }, 1000);
