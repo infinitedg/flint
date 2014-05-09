9bc617ae25112852d05f17017d3730ea6f43dd11
Alex <alexanderson1993@gmail.com>
2014-05-08 23:20:05 -0600

    Ramped up hyperspace to make it a little cooler. TODO: Add a cool entering hyperspace animation.

--- packages/card-sensor3d/core.js
+++ packages/card-sensor3d/core.js
@@ -210,7 +210,7 @@ Template.core_sensor3d.created = function() {
                 z = 0,
                 d = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2));
             if (d < 1.2) { // Only drop the contact if we are within 120% of the grid's radius
-              var updateObj = _.extend(cTmpl, {isMoving: true, isVisible: true, mesh: 'AstraShuttle', velocity: 0.1});
+              var updateObj = _.extend(cTmpl, {isMoving: true, isVisible: true, mesh: 'AstraShuttle', velocity: 0.05});
               updateObj['dst'+ currentDimensions.x.toUpperCase()] = x;
               updateObj['dst' + currentDimensions.y.toUpperCase()] = y;
               updateObj['dst' + currentDimensions.otherDimension().toUpperCase()] = z;
--- packages/card-viewscreen/package.js
+++ packages/card-viewscreen/package.js
@@ -1,16 +1,15 @@
 Package.describe({
 });
 
-Package.on_use(function(api) {  
+Package.on_use(function(api) {
   api.use(['templating', 'flint']);
-  
+
   api.add_files(['core.html', 'core.js'], 'client');
   api.add_files(['card.html', 'card.js', 'card.css'], 'client');
-    //api.add_files(['OrbitControls.js'], 'client');
     api.add_files(['models/Battleship/_1.mtl', 'models/Battleship/_1.obj','models/Battleship/battleship_elements2_c.png'], 'client');
     api.add_files(['models/AstraShuttle/_1.obj','models/AstraShuttle/_1.mtl', 'models/AstraShuttle/astra_elements1_c.png'], 'client');
     api.add_files(['models/AstraHeavyCruiser/_1.obj','models/AstraHeavyCruiser/_1.mtl', 'models/AstraHeavyCruiser/astra_elements1_c.png'], 'client');
     api.add_files(['models/AstraLightCruiser/_1.obj','models/AstraLightCruiser/_1.mtl', 'models/AstraLightCruiser/astra_elements1_c.png'], 'client');
     api.add_files(['models/starback.png', 'models/starfield.jpg',  'models/Planets/earthmap1k.jpg', 'models/Planets/earthbump1k.jpg', 'models/Planets/earthspec1k.jpg'], 'client');
-    api.add_files(['textures/water.jpg', 'textures/lensflare/lensflare0.png', 'textures/lensflare/lensflare2.png', 'textures/lensflare/lensflare3.png'], 'client');
+    api.add_files(['textures/water.jpg', 'textures/lensflare/lensflare0.png', 'textures/lensflare/lensflare2.png', 'textures/lensflare/lensflare3.png', 'textures/spikey.png'], 'client');
 });
