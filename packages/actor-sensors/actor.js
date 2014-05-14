a9ba1f83eec2cd90ba2cc6d616b3c5615a1c3e45
Brent Anderson <me@brentjanderson.com>
2014-05-14 16:46:03 -0600

    Updated to still use TWEEN.js but fixed weird multiple-tween bug

--- packages/actor-sensors/actor.js
+++ packages/actor-sensors/actor.js
@@ -1,4 +1,5 @@
-var defaultVelocity = 0.01;
+var defaultVelocity = 0.01,
+tweenBank = {};
 
 var a = Flint.actor({
 	_id:"actor-sensors",
@@ -18,12 +19,13 @@ var a = Flint.actor({
 	}
 });
 
-function findTweenById(id) {
-	return _.where(TWEEN.getAll(), {contactID: id})[0];
-}
-
 var updateTweener = function(doc) {
-	TWEEN.remove(findTweenById(doc._id));
+	var oldTween = tweenBank[id];
+	if (oldTween) {
+		oldTween.stop();
+		TWEEN.remove(oldTween);
+		delete tweenBank[doc._id];
+	}
 	var dist = Math.sqrt(Math.pow(doc.x - doc.dstX, 2) + Math.pow(doc.y - doc.dstY, 2) + Math.pow(doc.z - doc.dstZ, 2)),
 	velocity = doc.velocity || defaultVelocity;
 	var t = new TWEEN.Tween({x: doc.x, y: doc.y, z: doc.z})
@@ -37,30 +39,25 @@ var updateTweener = function(doc) {
 				Flint.collection('sensorContacts').update(doc._id, { $set: {x: this.x, y: this.y, z: this.z}});
 			}
 		})
-		.onStart(function() {
-			this.contactID = doc._id;
-		})
 		.onComplete(function() {
 			Flint.collection('sensorContacts').update(doc._id, {$set: {isMoving: false}});
 		})
 		.start();
+	tweenBank[doc._id] = t;
 }
 
 var observer = Flint.collection('sensorContacts').find({isMoving: true}).observe({
 	added: function(doc) {
-		// console.log("Added",doc._id);
 		if (doc.x !== doc.dstX || doc.y !== doc.dstY || doc.z !== doc.dstZ) {
 			updateTweener(doc);
 		}
 	},
 	changed: function(newDoc, oldDoc) {
 		if (newDoc.dstX !== oldDoc.dstX || newDoc.dstY !== oldDoc.dstY || newDoc.dstZ !== oldDoc.dstZ || newDoc.velocity !== oldDoc.velocity) {
-			// console.log("Changed",newDoc._id, newDoc);
 			updateTweener(newDoc);
 		}
 	},
 	removed: function(doc) {
-		// console.log("Removed",doc._id);
-		TWEEN.remove(findTweenById(doc._id));
+		TWEEN.remove(tweenBank[id]);
 	}
 });
