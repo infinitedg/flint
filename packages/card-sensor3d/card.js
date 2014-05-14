f321581cec9952fd2f9f18d6256274854405dfbd
Brent Anderson <me@brentjanderson.com>
2014-05-14 17:22:49 -0600

    Crazy radar animation.

--- packages/card-sensor3d/card.js
+++ packages/card-sensor3d/card.js
@@ -115,20 +115,40 @@ Template.card_sensor3d.rendered = function() {
 	// "Radar" axes
 	var radar_x = new THREE.Object3D(),
 		radar_y = new THREE.Object3D(),
+		radar_z = new THREE.Object3D(),
 		radar_length = viewRadius / 2,
 		radar_rotation_speed = 0.01;
 
 	radar_x.add(buildHalo( radar_length, 0xFF0000, new THREE.Vector3(1, 0, 0))); // +X
-    
     radar_y.add(buildHalo( radar_length, 0x00FF00, new THREE.Vector3(0, 1, 0))); // +Y
+	radar_z.add(buildHalo( radar_length, 0x0000FF, new THREE.Vector3(0, 0, 1))); // +Z
+	
+	radar_x.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( radar_length, 0, 0 ), 0xFF0000, false ) ); // +X
+    radar_x.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( -radar_length, 0, 0 ), 0xFF0000, true) ); // -X
     
+    radar_y.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, radar_length, 0 ), 0x00FF00, false ) ); // +Y
+    radar_y.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, -radar_length, 0 ), 0x00FF00, true ) ); // -Y
+    
+    radar_z.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, 0, radar_length ), 0x0000FF, false ) ); // +Z
+    radar_z.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, 0, -radar_length ), 0x0000FF, true ) ); // -Z
+
+
 	scene.add(radar_x);
 	scene.add(radar_y);
+	scene.add(radar_z);
 	
 	onRenderFcts.push(function() {
 		radar_x.rotation.x = radar_x.rotation.x + radar_rotation_speed;
-		
+		radar_x.rotation.y = radar_x.rotation.y + radar_rotation_speed;
+		radar_x.rotation.z = radar_x.rotation.z + radar_rotation_speed;
+
+		radar_y.rotation.x = radar_y.rotation.x + radar_rotation_speed;
 		radar_y.rotation.y = radar_y.rotation.y + radar_rotation_speed;
+		radar_y.rotation.z = radar_y.rotation.z + radar_rotation_speed;
+		
+		radar_z.rotation.x = radar_z.rotation.x + radar_rotation_speed;
+		radar_z.rotation.y = radar_z.rotation.y + radar_rotation_speed;
+		radar_z.rotation.z = radar_z.rotation.z + radar_rotation_speed;
 	});
 
 	// Animation Function
