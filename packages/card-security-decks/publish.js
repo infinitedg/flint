75d3da5a367970617ee163d5c4ab51cac91284cf
Brent Anderson <me@brentjanderson.com>
2014-05-14 19:02:35 -0600

    Add fixtures & card definitions for stations.

--- packages/card-security-decks/publish.js
+++ packages/card-security-decks/publish.js
@@ -1,1 +1,7 @@
-// stub
+Meteor.publish('cards.security-decks.allDecks', function(simulatorId) {
+	return Flint.collection('decks').find({simulatorId: simulatorId});
+});
+
+Meteor.publish('cards.security-decks.oneDeck', function(simulatorId, floor) {
+	return Flint.collection('decks').find({simulatorId: simulatorId, floor: floor});
+});
