f955377f8e4a0f25e1645dc989be69e89f954108
Brent Anderson <me@brentjanderson.com>
2014-06-18 22:00:48 -0600

    Tweaks and demonstration of localization

--- packages/flint/client/localization.js
+++ packages/flint/client/localization.js
@@ -1,5 +1,7 @@
 Meteor.startup(function(){
-	Meteor.subscribe("flint.localization", Flint.locale());
+	Deps.autorun(function() {
+		Meteor.subscribe("flint.localization", Flint.locale());
+	});
 	UI.registerHelper('t', function(sourceString) {
 		return Flint.t(sourceString);
 	});
