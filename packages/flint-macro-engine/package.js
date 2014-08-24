31431fd09acc17e0a93c90db3e1c949d18881bdf
Brent Anderson <me@brentjanderson.com>
2014-08-23 22:24:04 -0600

    Initial macro commit

---
+++ packages/flint-macro-engine/package.js
@@ -0,0 +1,12 @@
+Package.describe({
+  summary: 'Arbitrary macro execution engine',
+  version: "0.1.0"
+});
+
+Package.on_use(function(api) {  
+  
+  api.use(['flint'],['server', 'client']);
+
+  api.add_files(['server.js'], ['server']);
+  api.add_files(['client.js'], ['client']);
+});
