46fe8b64f21006438193b145a6fe949678ea1cc7
Alex Anderson <alexanderson1993@gmail.com>
2014-08-25 15:54:58 -0600

    Added themes for voyager and odyssey

--- packages/theme-odyssey/package.js
+++ packages/theme-odyssey/package.js
@@ -4,6 +4,6 @@ Package.describe({
 });
 
 Package.on_use(function(api) { 
-	api.use(['templating','flint']);
+	api.use(['templating', 'flint']);
 	api.add_files(['theme.html','theme.js'], ['client']);
 });
