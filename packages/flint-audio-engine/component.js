0456e8b00b0584f3234a4a89105fb02135e7fb14
Brent Anderson <me@brentjanderson.com>
2014-08-27 11:07:07 -0600

    Working sound implementation

--- packages/flint-audio-engine/component.js
+++ packages/flint-audio-engine/component.js
@@ -14,7 +14,6 @@ sounds:
 	soundGroups			- Array of sound groups (Converted into soundPlayers when inserted) (Optional)
 	keyId:				- key that originally scheduled this sound (Optional)
 	clientId:			- Client that originally scheduled this sound (Optional)
-	_soundState			- Internal flag used to determine the sound's state
 */
 
 // Used to track individual sounds playing back
@@ -34,11 +33,75 @@ Meteor.startup(function() {
 	Meteor.subscribe('flint.audio-engine.sounds');
 });
 
+function refreshSound(sound) {
+	// Sound can be an object or a string
+	// If a string, we assume it's the sound's ID and we go looking
+	if (typeof sound === "string") {
+		var soundObj = Flint.collection('flintSounds').findOne(sound);
+		if (!soundObj) {
+			Flint.Log.error("Attempt to refresh unknown sound " + sound, "flint-audio-engine");
+			return;
+		}
+		sound = soundObj;
+	}
+
+	_buzzSoundCache[sound._id].setVolume(sound.volume || 100);
+
+	if (sound.looping) {
+		_buzzSoundCache[sound._id].setTime(0);
+		_buzzSoundCache[sound._id].loop();
+	} else {
+		_buzzSoundCache[sound._id].unloop();
+	}
+
+	if (sound.muted) {
+		_buzzSoundCache[sound._id].mute();
+	} else {
+		_buzzSoundCache[sound._id].unmute();
+	}
+
+	if (sound.paused) {
+		_buzzSoundCache[sound._id].pause();
+	} else {
+		_buzzSoundCache[sound._id].play();
+	}
+
+}
+
 Template.comp_flint_player.created = function() {
 	this.playerSub = Flint.collection('flintSounds').find({ soundPlayers: { $in: [Flint.clientId()] }, parentSounds: {$size: 0} }).observe({
-		added: function(doc) {
-			// Play sounds
-			console.log(doc);
+		added: function(sound) {
+			var asset = Flint.a(sound.assetKey);
+			if (asset) {
+				if (isNaN(parseFloat(sound.delay)) || !isFinite(sound.delay)) {
+					sound.delay = 0;
+				}
+				Meteor.setTimeout(function() {
+					_buzzSoundCache[sound._id] = new buzz.sound(asset, {
+						loop: sound.looping || false,
+						autoplay: false
+					});
+					_buzzSoundCache[sound._id].load();
+
+					_buzzSoundCache[sound._id].bindOnce("ended", function() {
+						Flint.collection('flintSounds').remove(sound._id);
+					});
+
+					refreshSound(sound);
+				}, sound.delay * 1000);
+			} else {
+				Flint.Log.warn("Unable to find asset for sound playback " + sound.assetKey + "; removing sound", "flint-audio-engine");
+				Flint.collection('flintSounds').remove(sound._id);
+			}
+		},
+		changed: function(sound) {
+			refreshSound(sound);
+		},
+		removed: function(sound) {
+			if (_buzzSoundCache[sound._id]) {
+				_buzzSoundCache[sound._id].stop();
+				delete _buzzSoundCache[sound._id];
+			}
 		}
 	});
 };
