a66a0da0ab05eb105c699b86314b0bfaebcf86ad
Alex Anderson <alexanderson1993@gmail.com>
2014-08-30 18:22:54 -0600

    First work on the transporters. Refs #84

---
+++ packages/card-transporters/package.js
@@ -0,0 +1,11 @@
+Package.describe({
+  "summary": "Odyssey-type transporters",
+  version: "0.1.0"
+});
+
+Package.on_use(function(api) {  
+  api.use(['templating', 'flint']);
+  
+  api.add_files(['core.html', 'core.js'], 'client');
+  api.add_files(['card.html', 'card.js', 'card.css'], 'client');
+});
