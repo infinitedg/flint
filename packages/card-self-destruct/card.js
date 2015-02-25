a69dff5073630e9452c5eaa606b7f903d51924ec
Alex Anderson <alex@infinitedev.com>
2015-02-24 20:03:51 -0700

    Self-destruct bug fixes

--- packages/card-self-destruct/card.js
+++ packages/card-self-destruct/card.js
@@ -1,58 +1,56 @@
 var countdownEnd = new Date();
 
 Template.card_selfDestruct.events = {
-    'mouseup .modalOpen': function(e, context) {
-        if (! Template.card_selfDestruct.currentCountdown()){
-              bootbox.prompt('ENTER TIME UNTIL SELF DESTRUCT IN MINUTES:', function(result){
-                  if (result === null || isNaN(result)) {                                             
+  'mouseup .modalOpen': function(e, context) {
+    if (! currentCountdown()){
+      bootbox.prompt('ENTER TIME UNTIL SELF DESTRUCT IN MINUTES:', function(result){
+        if (result === null || isNaN(result)) {                                             
                       //Example.show("Prompt dismissed");                              
-                  } else {
+                    } else {
                       //Example.show("Hi <b>"+result+"</b>");
-                    var selfDestruct = new Countdown({  
+                      var selfDestruct = new Countdown({  
                         seconds:(result * 60),  // number of seconds to count down
                         onUpdateStatus: function(sec){
-                            parseTimer(sec);
-                            
+                          parseTimer(sec);
                         }, // callback for each second
                         //onCounterEnd: function(){ $('.bigBorder').removeClass('animating');} // final action
-                    });
+                      });
                       selfDestruct.start();
-                  }
-                  
-              });
-        } else{
-           bootbox.confirm("Would you like to deactivate self destruct?", function(result) {
-               if (result === true) {
-                   countdownEnd = '';
-                   Meteor.clearInterval(timer);
-                    Flint.simulators.update(Flint.simulatorId(), {$set: {selfDestructCountdown: null}});
-               }
-           }); 
-        }
-    },
-    
-    'keypress input.input-block-level': function(e, t) {
-        console.log("gotit!");
-            if (e.which === 13) {
-                // Simulate click event
-                var evObj = document.createEvent('Events');
-                evObj.initEvent('click', true, false);
-                t.find('.modal-footer .btn.btn-primary').dispatchEvent(evObj);
-    
-                e.preventDefault();
-                return false;
-            }
-            return true;
-        }
-    
+                    }
+                  });
+    } else{
+     bootbox.confirm("Would you like to deactivate self destruct?", function(result) {
+       if (result === true) {
+         countdownEnd = '';
+         Meteor.clearInterval(timer);
+         Flint.simulators.update(Flint.simulatorId(), {$set: {selfDestructCountdown: null}});
+       }
+     }); 
+   }
+ },
+
+ 'keypress input.input-block-level': function(e, t) {
+  if (e.which === 13) {
+    // Simulate click event
+    var evObj = document.createEvent('Events');
+    evObj.initEvent('click', true, false);
+    t.find('.modal-footer .btn.btn-primary').dispatchEvent(evObj);
+
+    e.preventDefault();
+    return false;
+  }
+  return true;
+}
+
 };
 
+var currentCountdown = function(){
+  var t = Flint.simulator().selfDestructCountdown;
+  if (t == '00:00:00'){t = '';}
+  return t;
+}
 Template.card_selfDestruct.helpers({
-  currentCountdown: function(){
-      var t = Flint.simulator().selfDestructCountdown;
-      if (t == '00:00:00'){t = '';}
-      return t;
-  },
+  currentCountdown: currentCountdown,
   isOn: function(){
     var t = Flint.simulator().selfDestructCountdown; 
     if (t == '00:00:00' || t === '' || t === null) { 
@@ -64,15 +62,15 @@ Template.card_selfDestruct.helpers({
 });
 
 function parseTimer(currentTime){
-    var hours = Math.floor(currentTime/(60*60));
-    var minutes = Math.floor((currentTime - (hours*60*60))/60);
-    var seconds = Math.floor(currentTime - (hours*60*60 + minutes*60));
-    var countdownOutput = pad(hours) + ":" + pad(minutes) + ":" + pad(seconds);
-    Flint.simulators.update(Flint.simulatorId(), {$set: {selfDestructCountdown: countdownOutput}});
-                         
+  var hours = Math.floor(currentTime/(60*60));
+  var minutes = Math.floor((currentTime - (hours*60*60))/60);
+  var seconds = Math.floor(currentTime - (hours*60*60 + minutes*60));
+  var countdownOutput = pad(hours) + ":" + pad(minutes) + ":" + pad(seconds);
+  Flint.simulators.update(Flint.simulatorId(), {$set: {selfDestructCountdown: countdownOutput}});
+
 }
 function pad(n) {
-    return (n < 10) ? ("0" + n) : n;
+  return (n < 10) ? ("0" + n) : n;
 }
 var timer;
 
