a4564c527d645f452ab67bced4fb9a42654f925f
Alex Anderson <alex@infinitedev.com>
2015-02-25 10:34:28 -0700

    Major modification to the router and layout. Login card is no longer required for stations.

--- packages/flint/client/router.js
+++ packages/flint/client/router.js
@@ -23,12 +23,7 @@ Router.onBeforeAction(function () {
 });
 
 function currentCardId() {
-  // If we're not logged in, then take us to card 0 (presumably login)
-  if (Flint.station() && Flint.station().name != "Flint Admin"){
-    return (Flint.client() && Flint.client().name) ? this.params.cardId : 0;
-  } else {
-    return this.params.cardId;
-  }
+  return this.params.cardId;
 }
 
 Router.map(function () {
@@ -56,8 +51,8 @@ Router.map(function () {
     },
     subscriptions: function() {
       return [
-        Meteor.subscribe('flint.picker.simulator', this.params.simulatorId),
-        Meteor.subscribe('flint.picker.stations', this.params.simulatorId),
+      Meteor.subscribe('flint.picker.simulator', this.params.simulatorId),
+      Meteor.subscribe('flint.picker.stations', this.params.simulatorId),
       ];
     },
     onBeforeAction: function () {
@@ -67,14 +62,14 @@ Router.map(function () {
       this.next()
     }
   });
-  
+
   this.route('flint_station', {
     path: '/simulator/:simulatorId/station/:stationId/card/:cardId',
     subscriptions: function() {
       return [
-        Meteor.subscribe('flint.picker.simulator', this.params.simulatorId),
-        Meteor.subscribe('flint.picker.station', this.params.stationId),
-        Meteor.subscribe('flint.picker.systems', this.params.simulatorId)
+      Meteor.subscribe('flint.picker.simulator', this.params.simulatorId),
+      Meteor.subscribe('flint.picker.station', this.params.stationId),
+      Meteor.subscribe('flint.picker.systems', this.params.simulatorId)
       ];
     },
     onBeforeAction: function() {
@@ -86,11 +81,16 @@ Router.map(function () {
     action: function() {
       if (!Flint.station())
         return;
+      if (!Flint.client().name){
+        this.render('card_login');
+      } else if (Flint.client().locked){
+        this.render('card_locked');
+      } else {
+        var cardId = currentCardId.apply(this);
 
-      var cardId = currentCardId.apply(this);
-
-      var card = (Flint.station().cards) ? Flint.station().cards[cardId] : {cardId: 'flint_404'};
-      this.render(card.cardId);
+        var card = (Flint.station().cards) ? Flint.station().cards[cardId] : {cardId: 'flint_404'};
+        this.render(card.cardId);
+      }
     },
     data: function() {
       var cardId = currentCardId.apply(this);
--- packages/layout-digital/template.js
+++ packages/layout-digital/template.js
@@ -16,9 +16,19 @@ Template.digital_cardList.helpers({
 	currentCardImage: function(){
 		var cardId = Flint.card().cardId.replace(/_/g,'-');
 		return '/packages/' + cardId + "/icon.png";
+	},
+	locked: function(){
+		if (!Flint.client('name') || Flint.client('locked')){
+			return 'locked';
+		}
 	}
 })
 
+Template.digital_cardList.events({
+	'click .lockScreen':function(){
+		Flint.client('locked',true);
+	}
+})
 Template.layout_digital.helpers({
 	retina: function(){
 		var retina = "only screen and (-webkit-min-device-pixel-ratio: 2)," + "only screen and (min--moz-device-pixel-ratio: 2)," + "only screen and (-o-min-device-pixel-ratio: 2/1)," + "only screen and (min-device-pixel-ratio: 2)," + "only screen and (min-resolution: 192dpi)," + "only screen and (min-resolution: 2dppx)"
@@ -28,6 +38,39 @@ Template.layout_digital.helpers({
 	alertColor: function(){
 		var alertNum = Flint.simulator('alertCondition');
 		return 'alertColor' + alertNum;
+	},
+	alertnum:function(){
+		if (Flint.simulator()){
+			return Flint.simulator('alertCondition');
+		}
+	},
+})
+
+Template.digital_nameBox.helpers({
+	username: function(){
+		if (Flint.client()){
+			return Flint.client().name;
+		}
+	},
+	usernameStyle: function(){
+		if (Flint.client()){
+			if (Flint.client().name){
+				if (Flint.client().name.length > 12){
+					return "font-size:" + (20 - Flint.client().name.length/2) + "px;";
+				}
+			}
+		}
+	},
+	stationName: function(){
+		if (Flint.station()){
+			return Flint.station().name;
+		}
+	},
+	stationNameStyle: function(){
+
+	},
+	profilePictureSrc: function(){
+		return "https://infinitedev-flint.s3.amazonaws.com/flintassets/xfNRX3Kb9Cp4pYi4t-flintassets-zckr6cCsbfzF5YFeH-flintassets-E2k4ewQA7RPJGhnGh-Vanguard.png";
 	}
 })
 Router.onBeforeAction(function(){
