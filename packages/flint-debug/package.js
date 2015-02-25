fdfaa20a704f26fd9db8bbc080c8023d99125f76
Alex Anderson <alex@infinitedev.com>
2015-02-24 20:03:18 -0700

    Added Mongol debugger tool.

--- packages/flint-debug/package.js
+++ packages/flint-debug/package.js
@@ -1,10 +1,11 @@
 Package.describe({
   summary: "Front-end interfaces for things not normally visible to users.",
-  version: "0.1.0"
+  version: "0.1.0",
+  debugOnly: true
 });
 
 Package.on_use(function(api) {  
-  api.use(['templating']);
+  api.use(['templating','msavin:mongol']);
   
   api.add_files(['debugging.html', 'debugging.js'], 'client');
 });
